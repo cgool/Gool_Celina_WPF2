a54e08282e514c96ba3057b344968c88995a7f66
Celina Gool_ <celina.gool@gmail.com>
2014-12-12 01:11:34 -0500

    New Repository For Class
    I could not sync my work to my previous repositories.

---
+++ Week3/GoToTrainingWeek3/js/week3.js
@@ -0,0 +1,21 @@
+/*
+ Dan Williams
+ SDI Section #3
+ Go To Training Week #3
+ 12-10-2014
+ */
+
+//alert("Testing To Make Sure we are working.");
+
+//Create code that sorts through of fruit names and picks out pears
+
+//Create our "fruit bowl" array
+
+var bowlOfFruit = ["apple", "banana","pear", "peach"];
+
+//Console logs the WHOLE array
+console.log(bowlOfFruit);
+
+//How can i get one fruit inside the bowl.
+
+console.log(bowlOfFruit[1]);
